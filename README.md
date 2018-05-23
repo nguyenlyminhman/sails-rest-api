42c2aa2fae25b30501592a8d01272f3cda4605b2
nguyenlyminhman <nguyenlyminhman@gmail.com>
2018-05-23 11:34:12 +0700

    Init the sails project.

---
+++ README.md
@@ -0,0 +1,8 @@
+# sails-rest-api
+
+a [Sails v1](https://sailsjs.com) application
+
+### Version info
+
+This app was originally generated on Wed May 23 2018 11:03:55 GMT+0700 (SE Asia Standard Time) using Sails v1.0.2. Building on WindowOS
+
---
+++ api/controllers/PostController.js
@@ -0,0 +1,15 @@
+/**
+ * PostController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+    archive: function (req,res){
+        return res.ok({smg: "ok man"})
+    }
+
+};
+
